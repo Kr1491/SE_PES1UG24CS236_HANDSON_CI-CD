70d2363ab0f1d9fd99c773e4871088602c731fcf
Krishna Jha <krishnajha@Krishnas-MacBook-Pro.local>
2026-09-16 11:22:14 +0530

    fail CI Build Demo

--- c-calculator/src/calculator.c
+++ c-calculator/src/calculator.c
@@ -1,6 +1,6 @@
 #include "calculator.h"
 #include <stdio.h>
-double add(double a, double b) { return a + b; }
+double add(double a, double b) { return a - b; }
 double subtract(double a, double b) { return a - b; }
 double multiply(double a, double b) { return a * b; }
 double divide(double a, double b) {
