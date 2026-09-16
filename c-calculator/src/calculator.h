c01d2e45214cb2b7b6d37c8c8ac077612f5d7d3e
Krishna Jha <krishnajha@Krishnas-MacBook-Pro.local>
2026-09-16 11:12:12 +0530

    initial project setup

---
+++ c-calculator/src/calculator.h
@@ -0,0 +1,8 @@
+#ifndef CALCULATOR
+#define CALCULATOR
+
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+#endif
